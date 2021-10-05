d0de31a017d68406e531064c5ee00b1f99a8c990
gypsyrossel <gypsyrossel@gmail.com>
2021-10-05 18:56:07 -0300

    migración y controladores

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :movies
-  root "articles#index"
-  get "/articles", to: "articles#index"
+  root "movie#index"
+  get "/movie", to: "movie#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+for m in 1..100 do
+ name = Faker::Movie.title
+ director = Faker::Name.name
+ summary = Faker::Lorem.sentence
+ Movie.create({name: name, director: director, summary: summary})
+end
